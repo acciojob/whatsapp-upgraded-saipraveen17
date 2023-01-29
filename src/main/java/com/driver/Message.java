12ef7d7486af6ffa6ce4876e41ad7dfaf9132155
Praveen <praveenk9417@gmail.com>
2023-01-29 12:58:37 +0530

    Submission 2

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -10,6 +10,11 @@ public class Message {
     public Message() {
     }
 
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
